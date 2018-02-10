06682586cb75aea0a7b856623c7276f2856902db
pkmangliik <pkmangliik@gmail.com>
2018-02-10 12:34:09 -0500

    commit from dev branch

--- maven-demo/src/main/java/com/manglik/ManglikApp.java
+++ maven-demo/src/main/java/com/manglik/ManglikApp.java
@@ -5,5 +5,9 @@ public class ManglikApp
 	public static void main(String[] args) 
 	{
 		System.out.println("This is first class which is committed from 2 system in GIT");
+		MavenDemo mavenDemo = new MavenDemo();
+		
+		System.out.println(mavenDemo.firstMavenDemo());
+		System.out.println("these chnge ar from dev");
 	}
 }
