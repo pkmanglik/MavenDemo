dc52b45f6bc1a0f36be63aeacd9e3d87e1143d8c
prashantmanglik <prashant.maglik@accenture.com>
2018-02-10 00:59:31 -0500

    first Commit fro MavenDemo

---
+++ maven-demo/src/main/java/com/manglik/MavenDemo.java
@@ -0,0 +1,11 @@
+package com.manglik;
+
+public class MavenDemo 
+{
+
+	public String firstMavenDemo()
+	{
+		return "Maven Used as Build";
+	}
+	
+}
---
+++ maven-demo/src/test/java/com/manglik/MavenDemoTest.java
@@ -0,0 +1,14 @@
+package com.manglik;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MavenDemoTest {
+
+	@Test
+	public void test() {
+	
+	}
+
+}
