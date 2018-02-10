205acade6f32838b4a5b8b59b4e6846106846998
pkmangliik <pkmangliik@gmail.com>
2018-02-10 12:15:53 -0500

    first commit from secondary machine

---
+++ maven-demo/src/main/java/com/manglik/ManglikApp.java
@@ -0,0 +1,9 @@
+package com.manglik;
+
+public class ManglikApp 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("This is first class which is committed from 2 system in GIT");
+	}
+}
